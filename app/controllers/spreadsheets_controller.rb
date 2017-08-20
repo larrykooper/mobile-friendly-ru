2e4fb39d04e26a31650e9c740f73ceaaf6fb2d32
larrykooper <larry.kooper@gmail.com>
2017-08-20 13:21:28 -0400

    Rubocop fixes

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -1,22 +1,16 @@
+# The Spreadsheets Controller is executed when the user enters the app.
+# It asks for data from Google.
 class SpreadsheetsController < ApplicationController
-
   def getdata
-
     @data = SpreadsheetReader.get_sheet_data
     unless @data
       redirect_to '/auth/google_oauth2' and return
     end
 
     respond_to do |format|
-      format.html {
-        render 'spreadsheets/index'
-      }
-      format.json {
-          render json: @data
-      }
-      format.text {
-          render :text => @data
-      }
+      format.html { render 'spreadsheets/index' }
+      format.json { render json: @data }
+      format.text { render :text => @data }
     end
   end
 
