6d108bda49ca7609a403354ed3a324bd0d9339e8
larrykooper <larry.kooper@gmail.com>
2017-08-20 14:52:17 -0400

    Deleted unneeded code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'spreadsheets#getdata'
-  resources :sessions, only: :index
   get '/auth/:provider/callback' => 'sessions#create'
   get 'spreadsheet/getdata' => 'spreadsheets#getdata'
 end
