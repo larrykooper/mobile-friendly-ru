7052336e2cdd56f9c70e939601c23fcb3f7e9881
larrykooper <larry.kooper@gmail.com>
2017-08-20 13:59:49 -0400

    fixed omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider
-    :google_oauth2, ENV['MOBILE_RU_CLIENT_ID'],
-      ENV['MOBILE_RU_CLIENT_SECRET'],
+  provider :google_oauth2, ENV['MOBILE_RU_CLIENT_ID'],       ENV['MOBILE_RU_CLIENT_SECRET'], {
     scope: ['email', 'https://www.googleapis.com/auth/drive',
       'https://spreadsheets.google.com/feeds/']
+  }
 
   # provider: Tells Omniauth to initialize the google_oauth2 strategy
   #  with my client_id and client_secret
