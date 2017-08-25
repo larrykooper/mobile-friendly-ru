0084c5b674fad5cd66b4e05adaffe0e5b2fdcba0
larrykooper <larry.kooper@gmail.com>
2017-08-24 20:47:14 -0400

    added need for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 # ApplicationController is the base class for all the application's controllers.
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
 end
