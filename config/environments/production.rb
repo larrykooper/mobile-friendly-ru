8d936ba358c020765067682a3b5a7fc7771a8d8d
larrykooper <larry.kooper@gmail.com>
2017-08-24 20:45:35 -0400

    Added action mailer defaults

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'warm-retreat-30658.herokuapp.com' }
 end
