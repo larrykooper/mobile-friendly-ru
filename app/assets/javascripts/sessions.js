16e7f67e29aae119070058ac6d53bdcffa4ece46
larrykooper <larry.kooper@gmail.com>
2017-08-30 20:01:44 -0400

    sorting the list- first cut

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(document).ready(function() {
   // jQuery v 1.12.4
 
@@ -5,5 +7,33 @@ $(document).ready(function() {
     $('#sorting-dialog').modal();
   });
 
+  $('.btn-primary').click(sortRows);
 
 });  // end- document.ready
+
+
+function sortRows() {
+  var $rows = $('.row');
+  $rows.sort(compareFunction);
+  $('.rows-container').html($rows);
+}
+
+function compareFunction(row1, row2) {
+  var comp1 = getInOut(row1);
+  var comp2 = getInOut(row2);
+  if (comp1 > comp2) {
+    return 1;
+  };
+  if (comp1 < comp2) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+function getInOut(row) {
+  var $j = $(row);
+  var text =  $j.find('.outside-inside').html();
+  return text.trim();
+}
+
