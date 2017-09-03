deacc17637213e21bfac317a4568ef7e74e8e3d3
larrykooper <larry.kooper@gmail.com>
2017-09-03 15:08:31 -0400

    Moved arrow to 'public'

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,6 @@ module MobileFriendlyRu
     config.load_defaults 5.1
     config.autoload_paths << Rails.root.join('lib')
 
-    config.assets.precompile += [left_arrow.svg]
-
     # Settings in config/environments/* take precedence
     #   over those specified here.
     # Application configuration should go into files in config/initializers
